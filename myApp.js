de7fbd64e75379f7b2eef586f05b1cf4d9398045
Ebo Mutele <45973019+EboMutele@users.noreply.github.com>
2022-10-16 16:29:38 +0000

    Can now serve json in a specific route.

--- myApp.js
+++ myApp.js
@@ -1,5 +1,22 @@
 let express = require('express');
 let app = express();
+console.log("Hello World");
+
+//cssStyle = __dirname + "/public/style.css";
+//htmlViews= __dirname + "/views/index.html";
+
+
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/views/index.html"); 
+  
+});
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get('/json', (req, res) => {
+  res.json({"message": process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json"});
+});
 
 
 
